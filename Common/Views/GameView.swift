ebdcb00ebb11d9a535d1405e9abef2077d8b5982
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-04-10 00:12:53 +0200

    ⚰️ Removed old unused code

--- Common/Views/GameView.swift
+++ Common/Views/GameView.swift
@@ -7,20 +7,6 @@ public class GameView: SKView, SKSceneDelegate {
     
     private(set) var map: MapNode!
     
-    private var character: GKEntity!
-    private var characterComponent: CharacterComponent!
-    
-    internal var debugNode: SKLabelNode!
-    
-    private var lastTime: TimeInterval?
-    
-    private var pressedArrows: [Keycode:Bool] = [
-        .leftArrow: false,
-        .rightArrow: false,
-        .upArrow: false,
-        .downArrow: false
-    ]
-    
     override public init(frame frameRect: CGRect) {
         super.init(frame: frameRect)
 
@@ -42,42 +28,11 @@ public class GameView: SKView, SKSceneDelegate {
         
         unloadMap(removePreviousMap: false)
       
-        //self.initCharacter()
-        initDebug()
         initDefaultMaterials()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-//    func initCharacter() {
-//        let texture = Atlases.main["test_character"]
-//        let sprite = SKSpriteNode(texture: texture)
-//        sprite.anchorPoint = CGPoint(x: 0.5, y: 0)
-//        sprite.zPosition = 10000
-//        scene!.addChild(sprite)
-//
-//        character = GKEntity()
-//        character.addComponent(GKSKNodeComponent(node: sprite))
-//
-//        let minimapMarker = gameScene.childNode(withName: "//MinimapMarker")!
-//
-//        characterComponent = CharacterComponent(startPositionX: 1, y: 4, onMap: gameScene.map, miniMapNode: minimapMarker)
-//        character.addComponent(characterComponent)
-//    }
-
-    func initDebug() {
-        debugNode = SKLabelNode()
-        debugNode.position = CGPoint(x: -300, y: 150)
-        debugNode.zPosition = 10000
-        debugNode.numberOfLines = 0
-
-        debugNode.fontSize = 10
-        debugNode.verticalAlignmentMode = .top
-        debugNode.horizontalAlignmentMode = .left
-
-        gameScene.camera!.addChild(debugNode)
     }
     
     func initDefaultMaterials() {
@@ -91,78 +46,7 @@ public class GameView: SKView, SKSceneDelegate {
         try! Materials.register(material: defaultRightElevationMaterial, forName: "default_elevation_right")
     }
     
-    public func update(_ currentTime: TimeInterval, for scene: SKScene) {
-//        guard let lastTime = lastTime else {
-//            lastTime = currentTime
-//            return
-//        }
-//
-//        var x: CGFloat = 0
-//        if pressedArrows[.leftArrow]! {
-//            x -= 1
-//        }
-//        if pressedArrows[.rightArrow]! {
-//            x += 1
-//        }
-//
-//        var y: CGFloat = 0
-//        if pressedArrows[.upArrow]! {
-//            y += 1
-//        }
-//        if pressedArrows[.downArrow]! {
-//            y -= 1
-//        }
-//
-//        characterComponent.direction = CGPoint(x: x, y: y)
-//
-//        let timeDiff = currentTime - lastTime
-//        self.lastTime = currentTime
-//        character.update(deltaTime: timeDiff)
-//
-//        var debugMessage = String(format: "Map position: %.3f : %.3f\n", characterComponent.mapPosition.x, characterComponent.mapPosition.y)
-//        + String(format: "Integral part: %d : %d\n", characterComponent.integralX, characterComponent.integralY)
-//        + String(format: "Fractional part: %.3f : %.3f\n", characterComponent.fractionalX, characterComponent.fractionalY)
-//
-//        var transformedMapVector = CGPoint(x: characterComponent.fractionalX, y: characterComponent.fractionalY)
-//        transformedMapVector = transformedMapVector.applying(CGAffineTransform(rotationAngle: .pi * -45.0 / 180.0))
-//        transformedMapVector = transformedMapVector.applying(CGAffineTransform(scaleX: sqrt(2), y: sqrt(2)))
-//        debugMessage += String(format: "Transformed (unit): %.3f : %.3f\n", transformedMapVector.x, transformedMapVector.y)
-//        transformedMapVector = transformedMapVector.applying(CGAffineTransform(scaleX: CGFloat(MapNode.halfWidth), y: CGFloat(MapNode.halfHeight)))
-//        debugMessage += String(format: "Transformed (pixels): %.3f : %.3f\n", transformedMapVector.x, transformedMapVector.y)
-//
-//        debugNode.text = debugMessage
-    }
-    
-    override public func keyDown(with event: NSEvent) {
-        guard
-            !event.isARepeat,
-            let keycode = Keycode(rawValue: event.keyCode)
-        else {
-            return
-        }
-        
-        switch keycode {
-        case .leftArrow, .rightArrow, .downArrow, .upArrow:
-            pressedArrows[keycode] = true
-            break
-        default:
-            break
-        }
-    }
-    
-    override public func keyUp(with event: NSEvent) {
-        guard let keycode = Keycode(rawValue: event.keyCode) else {
-            return
-        }
-        
-        switch keycode {
-        case .leftArrow, .rightArrow, .downArrow, .upArrow:
-            pressedArrows[keycode] = false
-            break
-        default:
-            break
-        }
-    }
+    public func update(_ currentTime: TimeInterval, for scene: SKScene) { }
     
     public func loadMap(file: URL) {
         unloadMap()
