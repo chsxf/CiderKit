51b517b9c91ef78c6428cbe3ad717d50314920a3
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-07-20 07:09:56 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! ✨ Sprite Asset Editor

--- CiderKit-Editor/Views/SpriteAssetEditor/SpriteAssetEditorView.swift
+++ CiderKit-Editor/Views/SpriteAssetEditor/SpriteAssetEditorView.swift
@@ -1,15 +1,20 @@
 import SwiftUI
 import CiderKit_Engine
+import SpriteKit
 
 struct SpriteAssetEditorView: View {
     
     private let hostingWindow: NSWindow
     
-    @State private var selectedDatabase: String
+    @State private var selectedDatabaseId: String
+    @State private var selectedDatabase: SpriteAssetDatabase
+    @State private var zoom: Int = 4
     
     init(hostingWindow: NSWindow) {
         self.hostingWindow = hostingWindow
-        selectedDatabase = Project.current!.defaultSpriteAssetDatabase!.id
+        let defaultDB = Project.current!.defaultSpriteAssetDatabase!
+        selectedDatabase = defaultDB
+        selectedDatabaseId = defaultDB.id
     }
     
     fileprivate func dismiss(response: NSApplication.ModalResponse) {
@@ -17,9 +22,9 @@ struct SpriteAssetEditorView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             HStack {
-                Picker("Selected Database", selection: $selectedDatabase) {
+                Picker("Selected Database", selection: $selectedDatabaseId) {
                     ForEach(Project.current!.spriteAssetDatabases.keys.sorted(), id: \.self) { key in
                         if key != SpriteAssetDatabase.defaultDatabaseId {
                             Text(key)
@@ -30,6 +35,79 @@ struct SpriteAssetEditorView: View {
                 Button("Close") {
                     dismiss(response: .OK)
                 }
+            }
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Sprite Assets")
+                    List {
+                        ForEach(selectedDatabase.spriteAssets) { asset in
+                            Text(asset.name)
+                        }
+                    }
+                    .border(Color.gray, width: 1)
+                    HStack {
+                        Spacer()
+                        Button("+") {
+                            selectedDatabase.spriteAssets.append(SpriteAssetDescription(name: "Unnamed Sprite Asset"))
+                        }
+                        Button("-") {
+                            
+                        }
+                    }
+                }
+                .frame(width: 200)
+                
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Asset Hierarchy")
+                            List {
+                                
+                            }
+                            .border(Color.gray, width: 1)
+                            HStack {
+                                Spacer()
+                                Button("+") {
+                                    
+                                }
+                                Button("-") {
+                                    
+                                }
+                            }
+                        }
+                        .frame(width: 200)
+                        
+                        ZStack(alignment: .bottomTrailing) {
+                            SpriteView(scene: SKScene())
+                            HStack {
+                                Text("Current zoom: x\(zoom)")
+                                Button("+") {
+                                    zoom *= 2
+                                }
+                                Button("100%") {
+                                    zoom = 1
+                                }
+                                .disabled(zoom == 1)
+                                Button("-") {
+                                    if zoom > 1 {
+                                        zoom /= 2
+                                    }
+                                }
+                                .disabled(zoom == 1)
+                            }
+                            .padding()
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .border(Color.gray)
+                        
+                        VStack {
+                            
+                        }
+                        .frame(width: 200)
+                    }
+                }
+                .frame(maxWidth: .infinity)
             }
         }
         .padding()
--- CiderKit-Engine/Classes/SpriteAssets/SpriteAssetDescription.swift
+++ CiderKit-Engine/Classes/SpriteAssets/SpriteAssetDescription.swift
@@ -3,4 +3,8 @@ public struct SpriteAssetDescription: Identifiable, Codable {
     public var id: UUID = UUID()
     public var name: String
     
+    public init(name: String) {
+        self.name = name
+    }
+    
 }
